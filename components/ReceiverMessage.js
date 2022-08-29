860a06325334b8d78e03b7af2b1ac7118f3d953b
nico bazzoni <nicobazzoni@gmail.com>
2022-08-29 10:44:41 -0400

    messges are recievable on firestore now but not yet rendered on screen

---
+++ components/ReceiverMessage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, Button, ImageBackground, TouchableOpacity } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+
+
+const ReceiverMessage = ({message}) => {
+    return (
+        <View 
+        style={[tw`bg-blue-500 rounded-lg rounded-tl-none px-5 py-3 mx-3 ml-14 ` , alignSelf='flex-start', ]}
+        >
+        <Text style={tw`text white`}>{message.message}</Text>
+        
+        <Image 
+        style={tw`rounded-full h-16 w-16 mr-4`}
+        source={{uri: message.photoURL}}
+        
+        />
+        </View>
+    );
+}
+
+export default ReceiverMessage;
--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,25 +1,89 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TextInput, Button, KeyboardAvoidingView, TouchableWithoutFeedback, FlatList} from 'react-native'
 import React, {useState} from 'react'
 import  Header  from '../components/Header'
 import useAuth from '../hooks/useAuth'
 import getMatchedUserInfo from '../lib/getMatchedUserInfo'
 import { useRoute } from '@react-navigation/native'
+import tw from 'tailwind-react-native-classnames'
+
+import SenderMessage from '../components/SenderMessage'
+import ReceiverMessage from '../components/ReceiverMessage'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase'
+
+
 
 const MessageScreen = () => {
     const { params } = useRoute();
     const { user} = useAuth();
     const [input, setInput] = useState('');
+    const [messages, setMessages] = useState([]);
     
    const { matchDetails } = params;
 
-   
-console.log(getMatchedUserInfo( matchDetails.users, user.uid).displayName)
-  return (
-    <SafeAreaView> 
+    const sendMessage = () => {  
+      addDoc(collection(db, 'matches', matchDetails.id, 'messages'), {
+        timesstamp: serverTimestamp(),
+        userId: user.uid,
+        displayName: user.displayName,
+        photoURL: matchDetails.users[user.uid].photoURL,
+        message: input,
+      })
+
+      setInput('')
+
+    }
+  
+
+return (
+    <SafeAreaView style={tw`flex-1`} > 
         <Header 
-        title={getMatchedUserInfo(matchDetails?.users, user.uid).displayName} />
-      <Text>MessageScreen</Text>
+        title={getMatchedUserInfo(matchDetails?.users, user.uid).displayName} 
+        />
+
+        <KeyboardAvoidingView 
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={tw`flex-1`}
+        keyboardVerticalOffset={10}
+        
+        
+        >
+          <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}> 
+          <FlatList
+          data={messages}
+          style={tw`pl-4`}
+          keyExtractor={(item) => item.id}
+          renderItem={({item: message}) => 
+          message.user.id === user.uid ? (
+            <SenderMessage key={message.id} message={message} />
+          ) : (
+            <ReceiverMessage key={message.id} message={message} />
+          )} 
+
+          
+          />
+          </TouchableWithoutFeedback>
+      
+  
+
+      <View style={tw`flex-row justify-between bg-white items-center border-t border-gray-200 px-5 py-2`} >
+        <TextInput 
+        style={tw`h-10 text-lg`}
+        placeholder="Send Message..."  
+        onChangeText={setInput}
+        onSubmitEditing={sendMessage}
+        value={input}
+        
+        
+
+        />
+        <Button onPress={sendMessage} title="Send" />
+      </View>
+      </KeyboardAvoidingView>
+     
     </SafeAreaView>
+
+    
   )
 }
 
