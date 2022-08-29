513b59331e319fa4dd2c2cd94c6b6e3a5286715d
nico bazzoni <nicobazzoni@gmail.com>
2022-08-29 13:56:18 -0400

    UI improved with padding and margin bottom for text bubbles, now more evenly spaced

--- components/ReceiverMessage.js
+++ components/ReceiverMessage.js
@@ -6,13 +6,15 @@ import tw from 'tailwind-react-native-classnames';
 const ReceiverMessage = ({ message }) => {
     return (
         <View 
-        style={tw.style(`bg-blue-500 rounded-lg rounded-tl-none px-5 py-3 mx-3 ml-14 `, 
-        {alignSelf: 'flex-start'}) }
+        style={tw.style(`bg-blue-500 rounded-lg rounded-tl-none px-3 py-4 mx-3 ml-14 p-5 mb-5
+        
+         `, 
+        {alignSelf: 'flex-start' , borderRadius: 10}) }
         >
         <Text style={tw`text white`}>{message.message}</Text>
         
         <Image 
-        style={tw`h-12 w-12 rounded-full absolute top-0 -left-14`}
+        style={tw`h-12 w-12 rounded-full absolute top-0 -left-14 `}
         source={{uri: message.photoURL}}
         
         />
